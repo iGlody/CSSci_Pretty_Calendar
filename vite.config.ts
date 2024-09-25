a3de256b379fc5f99353cee70697056bb627e55d
iGlody <ilukaspesek@gmail.com>
2024-09-25 12:59:28 +0200

    vite config tweak

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-import { sveltekit_modal } from 'sveltekit-modal/vite'; //Add this import
-
 export default defineConfig({
-    plugins: [sveltekit_modal(), sveltekit()] //Add the `sveltekit_modal()` plugin
+    plugins: [sveltekit()] //Add the `sveltekit_modal()` plugin
 });
