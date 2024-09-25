4d02fbf16450e39c34affbf94b4918a8b9f6441c
iGlody <ilukaspesek@gmail.com>
2024-09-25 12:17:47 +0200

    tweak

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import sveltePreprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
