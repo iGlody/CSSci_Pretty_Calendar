2748d3596591467c5d76822f54883171bf318280
iGlody <ilukaspesek@gmail.com>
2024-10-07 12:06:57 +0200

    tweak

--- src/lib/tasks/supabaseSubscription.ts
+++ src/lib/tasks/supabaseSubscription.ts
@@ -1,4 +1,3 @@
-import { createClient } from '@supabase/supabase-js';
 import { fetchAndFilterCalendar, generateIcs } from '$lib/calendarUtils';
 import { updateCalendarData } from '$lib/dbHelpers';
 import ical from 'ical.js';
